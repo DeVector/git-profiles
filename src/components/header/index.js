369c314bc0c99cd8b0000c73f96e528b8a6a0413
DeVector <diniz.vito@gmail.com>
2022-06-06 16:17:42 -0300

    Add header e layout para o sistema de pesquisa

---
+++ src/components/header/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import * as S from "./styled";
+import useGit from "../../hooks/git-hooks";
+
+const Header = () => {
+    const { getUser } = useGit();
+    const [usernameForSearch, setUsernameForSearch] = useState();
+
+    const submitGetUser = () => {
+        if(!usernameForSearch) return;
+        return getUser(usernameForSearch);
+    };
+
+    return (
+        <header>
+            <S.Wrapper>
+                <input
+                    type={text}
+                    placeholder="Digite o usuario do git aqui"
+                    onChange={(event) => setUsernameForSearch(event.target.value)}
+                />
+                <button type="submit" onClick={submitGetUser}>
+                    <span>Buscar</span>
+                </button>
+            </S.Wrapper>
+        </header>
+    );
+};
+
+export default Header;
